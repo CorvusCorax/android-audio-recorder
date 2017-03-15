a02261bb27debcf69bab2191f942e1315b5022c4
Alexey Kuznetsov <axet@me.com>
2017-03-15 09:36:41 +0300

    update libs

--- app/src/main/java/com/github/axet/audiorecorder/activities/MainActivity.java
+++ app/src/main/java/com/github/axet/audiorecorder/activities/MainActivity.java
@@ -223,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String[] PERMISSIONS = new String[]{READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
     boolean permitted(String[] ss) {
-        if (Build.VERSION.SDK_INT < 15)
+        if (Build.VERSION.SDK_INT < 16)
             return true;
         for (String s : ss) {
             if (ContextCompat.checkSelfPermission(this, s) != PackageManager.PERMISSION_GRANTED) {
@@ -234,7 +234,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     boolean permitted() {
-        if (Build.VERSION.SDK_INT < 15)
+        if (Build.VERSION.SDK_INT < 16)
             return true;
         for (String s : PERMISSIONS) {
             if (ContextCompat.checkSelfPermission(this, s) != PackageManager.PERMISSION_GRANTED) {
