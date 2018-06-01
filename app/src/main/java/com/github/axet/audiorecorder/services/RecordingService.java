37b4af7fee5b3f260eb5d619076828bcc7d76d5e
Alexey Kuznetsov <axet@me.com>
2018-06-01 19:20:03 +0300

    fix proximity

--- app/src/main/java/com/github/axet/audiorecorder/services/RecordingService.java
+++ app/src/main/java/com/github/axet/audiorecorder/services/RecordingService.java
@@ -222,7 +222,7 @@ public class RecordingService extends Service {
                 .setContentTitle(title)
                 .setContentText(text)
                 .setTicker(title)
-                .setSmallIcon(R.drawable.ic_mic_24dp)
+                .setSmallIcon(R.drawable.ic_mic_notification)
                 .setContent(view);
 
         if (Build.VERSION.SDK_INT < 11) {
