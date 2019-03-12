9b7f819dfa839cd7bf170f74d294355cf6d16332
Alexey Kuznetsov <axet@me.com>
2019-03-12 12:25:32 +0300

    add utc time format

--- app/src/main/java/com/github/axet/audiorecorder/app/Storage.java
+++ app/src/main/java/com/github/axet/audiorecorder/app/Storage.java
@@ -8,14 +8,22 @@ import android.os.Build;
 import android.preference.PreferenceManager;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Storage extends com.github.axet.audiolibrary.app.Storage {
+
+    public static final SimpleDateFormat ISO8601Z = new SimpleDateFormat("yyyyMMdd'T'HHmmss'Z'", Locale.US) {{
+        setTimeZone(TimeZone.getTimeZone("UTC"));
+    }};
 
     public static String getFormatted(String format, Date date) {
         format = format.replaceAll("%s", SIMPLE.format(date));
         format = format.replaceAll("%I", ISO8601.format(date));
         format = format.replaceAll("%T", "" + System.currentTimeMillis() / 1000);
+        format = format.replaceAll("%U", ISO8601Z.format(date));
         return format;
     }
 
@@ -41,7 +49,7 @@ public class Storage extends com.github.axet.audiolibrary.app.Storage {
             return getNextFile(context, path, format, ext);
         } else if (s.equals(ContentResolver.SCHEME_FILE)) {
             File f = getFile(path);
-            if (!f.exists() && !f.mkdirs())
+            if (!f.exists() && !f.mkdirs() && !f.exists())
                 throw new RuntimeException("Unable to create: " + path);
             return Uri.fromFile(getNextFile(f, format, ext));
         } else {
@@ -58,7 +66,7 @@ public class Storage extends com.github.axet.audiolibrary.app.Storage {
 
         format = getFormatted(format, new Date());
 
-        if (!f.exists() && !f.mkdirs())
+        if (!f.exists() && !f.mkdirs() && !f.exists())
             throw new RuntimeException("Unable to create: " + f);
         return getNextFile(f, format, ext);
     }
